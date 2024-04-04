eb8c93e2ecf303b2d8a7233b9ffccc4ad95e929a
Rüveyda Koçaker <rvydkocaker@gmail.com>
2024-04-04 16:48:45 +0300

    update

---
+++ DataAccess/Concrete/EntityFramework/ReCapDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ReCapDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = LAPTOP-HHNSAKJD\SQLEXPRESS;Database = ReCapDb; Trusted_Connection = true");
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
